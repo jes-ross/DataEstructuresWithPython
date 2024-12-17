7d25ce2d594dddd6f01963ecea63e2a09b9c53cd
Jesús <jesusrosalessantana@gmail.com>
2024-12-17 10:52:56 +0000

    Finished the Stack part

--- STACK/stack.py
+++ STACK/stack.py
@@ -12,6 +12,23 @@ class Stack:
         return self.__items.pop()
     def readTop(self):
         return self.__items[len(self.__items)-1]
+    def readBottom(self):
+        auxiliaryStack = Stack()
+        while not (self.isEmpty()):
+            auxiliaryStack.stacking(self.withDrawing())
+        bottomValue = auxiliaryStack.readTop()
+        while not(auxiliaryStack.isEmpty()):
+            self.stacking(auxiliaryStack.withDrawing())
+        return bottomValue
+    def numberOfElements(self):
+        auxiliaryStack = Stack()
+        while not(self.isEmpty()):
+            auxiliaryStack.stacking(self.withDrawing())
+        elements = 0
+        while not(auxiliaryStack.isEmpty()):
+            self.stacking(auxiliaryStack.withDrawing())
+            elements += 1
+        return elements
     def showStack(self):
         print("Stack: ", self.__items, "<--TOP")
 
@@ -38,12 +55,25 @@ def stackSimulator():
                 print("The top is: ", stack.readTop())
         elif(option == "4"):
             if stack.isEmpty():
+                print("You can't do this. There are no elements")
+            else:
+                print("The bottom is: ", stack.readBottom())
+        elif(option == "5"):
+            if stack.isEmpty():
                 print("The stack is empty")
             else:
                 print("The stack is not empty")
-        elif(option == "5"):
-            stack.showStack()
         elif(option == "6"):
+            if stack.isEmpty():
+                print("There are no elements to count")
+            else:
+                print("The number of elements is: ", stack.numberOfElements())
+        elif(option == "7"):
+            if stack.isEmpty():
+                print("There are no elements to show")
+            else:
+                stack.showStack()
+        elif(option == "8"):
             end = True
 print("""
 *******************
@@ -53,9 +83,11 @@ Menu:
     1) Stack
     2) Wtihdarw
     3) Read top
-    4) Is empty?
-    5) Show stack
-    6) Exit
+    4) Read bottom
+    5) Is empty?
+    6) Number of elements
+    7) Show stack
+    8) Exit
 """)
 
 stackSimulator()
